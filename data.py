c70f1e2571b135f49c1777719fdf9db5be2f62d4
Ben Kraines <bkraines@gmail.com>
2024-11-08 16:30:35 -0500

    Move data function

--- data.py
+++ data.py
@@ -5,6 +5,11 @@ import xarray as xr
 
 import yfinance as yf
 
+# from data import get_factor_master, get_yf_data
+from util import xr_pct_change
+from stats import get_volatility_set, get_correlation_set
+
+
 def get_yf_data(asset_list: List[str]) -> xr.DataArray:
     df = (yf.download(asset_list)
             .rename_axis(index='date', columns=['ohlcv_type', 'asset'])
@@ -30,3 +35,22 @@ def get_factor_master(filename: str = 'factor_master.xlsx', sheet_name: str = 'r
     df = pd.read_excel(filename, sheet_name=sheet_name, index_col=index_col)
     df.index = pd.CategoricalIndex(df.index, categories=df.index, ordered=True)
     return df
+
+
+# def get_factor_data(asset_list: List[str], halflifes: List[int]) -> xr.Dataset:
+def get_factor_data(halflifes: List[int] = None) -> xr.Dataset:
+    if halflifes == None:
+        halflifes = [21, 63, 126, 252, 512]
+    
+    factor_master = get_factor_master()
+    asset_list = factor_master.index.to_list()
+    
+    ds = xr.Dataset()
+    ds['ohlcv'] = get_yf_data(asset_list) #.to_dataset(name='ohlcv')
+    ds['cret']  = ds['ohlcv'].sel(ohlcv_type='adj close')
+    ds['ret']   = xr_pct_change(ds['cret'], 'date')
+    ds['vol']   = get_volatility_set(ds['ret'], halflifes)
+    ds['corr']  = get_correlation_set(ds['ret'], halflifes)
+    
+    ds['asset'].attrs = factor_master.T
+    return ds
--- stats.py
+++ stats.py
@@ -4,9 +4,6 @@ from numpy import sqrt
 import pandas as pd
 import xarray as xr
 
-import yfinance as yf
-
-from data import get_factor_master, get_yf_data
 from util import xr_pct_change
 
 def get_ewm_corr(ret: xr.DataArray, halflife: int) -> xr.DataArray:
@@ -32,20 +29,3 @@ def get_correlation_set(ret: xr.DataArray, halflifes: List[int]) -> xr.DataArray
                      dim=pd.Index(halflifes, name='corr_type'))
 
 
-# def get_factor_data(asset_list: List[str], halflifes: List[int]) -> xr.Dataset:
-def get_factor_data(halflifes: List[int] = None) -> xr.Dataset:
-    if halflifes == None:
-        halflifes = [21, 63, 126, 252, 512]
-    
-    factor_master = get_factor_master()
-    asset_list = factor_master.index.to_list()
-    
-    ds = xr.Dataset()
-    ds['ohlcv'] = get_yf_data(asset_list) #.to_dataset(name='ohlcv')
-    ds['cret']  = ds['ohlcv'].sel(ohlcv_type='adj close')
-    ds['ret']   = xr_pct_change(ds['cret'], 'date')
-    ds['vol']   = get_volatility_set(ds['ret'], halflifes)
-    ds['corr']  = get_correlation_set(ds['ret'], halflifes)
-    
-    ds['asset'].attrs = factor_master.T
-    return ds
