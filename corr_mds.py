53a53312386dcd7ba471f78aa8aa6b98fa384236
Ben Kraines <bkraines@gmail.com>
2024-11-20 11:06:15 -0500

    Improve mds time series rotations,  whiskers to scatter, highlight hyperfactors

--- corr_mds.py
+++ corr_mds.py
@@ -1,6 +1,7 @@
 from typing import Union, Literal
 
-from numpy import sqrt, cos, sin, arctan2, array
+from numpy import sqrt, cos, sin, arctan2, array, identity
+import numpy as np
 import xarray as xr
 xr.set_options(keep_attrs=True,
                display_expand_data=False)
@@ -24,7 +25,8 @@ def prepare_correlation(corr: xr.DataArray, transformation=None, start_date=None
             .assign(date = lambda df: df['date'].astype(str)))
 
 
-def transform_coordinates(coordinates: pd.DataFrame, transformation_type=None, factor: str = 'SPY'):
+def transform_coordinates(coordinates: pd.DataFrame, transformation_type=None, factor: str = 'SPY', 
+                          factor_list = None, coordinates_initial: pd.DataFrame= None) -> pd.DataFrame:
     
     def get_rotation_matrix(theta):
         return array([[cos(theta), -sin(theta)],
@@ -39,20 +41,46 @@ def transform_coordinates(coordinates: pd.DataFrame, transformation_type=None, f
     if transformation_type is None:
         return coordinates
     
-    v = coordinates.loc[factor]
-    x, y = v
-        
     if transformation_type == 'rotate':
+        v = coordinates.loc[factor]
+        x, y = v
         theta = arctan2(y, x)
         return coordinates.pipe(rotate, -theta)
     
+    if transformation_type == 'rotate_initial':
+            v = coordinates_initial.loc[factor]
+            x, y = v
+            theta = arctan2(y, x)
+            return coordinates.pipe(rotate, -theta)
+    
     if transformation_type == 'normalize':
+        v = coordinates.loc[factor]
+        x, y = v
         theta = arctan2(y, x)
         length = sqrt(sum(v**2))
         return coordinates.pipe(rotate, -theta).div(length)
+    
+    if transformation_type == 'rotate_list':
+        thetas = {}
+        if factor_list is None:
+            factor_list = ['SPY', 'TLT']
+        
+        for factor in factor_list:
+            v = coordinates.loc[factor]
+            x, y = v
+            thetas[factor] = arctan2(y, x)
+        theta_avg = sum(thetas.values()) / len(thetas)
+        return coordinates.pipe(rotate, -theta_avg)
+        
 
 
-def multidimensional_scaling(correlation_matrix: pd.DataFrame, init=None) -> pd.DataFrame:
+def multidimensional_scaling(correlation_matrix: pd.DataFrame, init=None, random_state=42, n_init=4) -> pd.DataFrame:
+    # TODO: Will this accept dataarray?
+    # TODO: Does it preserve distance? YES, see mds_test.ipynb
+        # If not, enforce distance is economic.
+        # Should match sqrt(1-r^2). 
+        # Rotate by -avg(theta) of hyperfactors
+    
     """
     Perform multidimensional scaling on a correlation matrix.
 
@@ -72,8 +100,8 @@ def multidimensional_scaling(correlation_matrix: pd.DataFrame, init=None) -> pd.
     dissimilarity_matrix = sqrt(1 - correlation_matrix**2)
 
     # Pass n_init explicitly to suppress warning when init is not None:
-    n_init = 4 if init is None else 1 
-    embedding = MDS(dissimilarity='precomputed', random_state=42, n_init=n_init)
+    n_init = 100 # if init is None else 1 
+    embedding = MDS(dissimilarity='precomputed', random_state=random_state, n_init=n_init)
     coordinates = embedding.fit_transform(dissimilarity_matrix, init=init)
     
     return pd.DataFrame(coordinates, 
@@ -81,21 +109,44 @@ def multidimensional_scaling(correlation_matrix: pd.DataFrame, init=None) -> pd.
                         columns=pd.Index(['dim1', 'dim2'], name='dimension'))
 
 
-def mds_ts_df(corr: xr.DataArray, start_date = None, transformation=None, factor='SPY') -> pd.DataFrame:
+def mds_ts_df(corr: xr.DataArray, start_date = None, transformation=None, factor='SPY', **kwargs) -> pd.DataFrame:
     # TODO: Factor out corr_type
 
     dates = corr.sel(date=slice(start_date, None)).date.values
+    # coordinates = None
+    transformed = None
     coordinates = None
-    
+
+    if transformation == 'rotate_initial':
+        date = dates.max()
+        df = corr.sel(date=date, corr_type=63).to_pandas() # * 0 + np.identity(len(corr.asset))
+        coordinates = multidimensional_scaling(df, init=transformed) #init=coordinates) 
+        transformed_initial = transform_coordinates(coordinates, 'rotate', factor='SPY')
+        transformation = None
+    else:
+        transformed_initial = None
+        
+    # for date in dates:
+    #     df = corr.sel(date=date, corr_type=63).to_pandas()
+    #     coordinates = multidimensional_scaling(df, init=transformed) #init=coordinates) 
+    #     transformed = transform_coordinates(coordinates, transformation, factor=factor, coordinates_initial=transformed_initial)
+    #     mds_dict[date] = transformed
+    #     return (pd.concat(mds_dict)
+    #             .rename_axis(index=['date', df.index.name])
+    #             )
+
+  
     mds_dict = {}
     for date in dates:
-        df = corr.sel(date=date, corr_type=63).to_pandas()
-        coordinates = multidimensional_scaling(df, init=coordinates)
-        transformed = transform_coordinates(coordinates, transformation, factor=factor)
+        df = corr.sel(date=date, corr_type=63).to_pandas() # * 0 + np.identity(len(corr.asset))
+        coordinates = multidimensional_scaling(df, init=transformed_initial) #init=transformed) 
+        transformed = transform_coordinates(coordinates, transformation, factor=factor, factor_list=None, coordinates_initial=transformed_initial)
         mds_dict[date] = transformed
     return (pd.concat(mds_dict)
             .rename_axis(index=['date', df.index.name])
             )
+
+
 
 
 def draw_mds_ts(df: pd.DataFrame, tick_range: Union[None, float, Literal['auto']] = 'auto') -> go.Figure:
@@ -128,32 +179,42 @@ def draw_mds_ts(df: pd.DataFrame, tick_range: Union[None, float, Literal['auto']
     >>> fig.show()
     """
     
-    fig_animation = {'animation_frame': 'date', 'animation_group': 'asset'}  if 'date' in df.columns else {}
-    fig_format    = {'template': 'plotly_white', 'height': 750, 'width': 750}
+    args_animation = {'animation_frame': 'date', 'animation_group': 'asset'}  if 'date' in df.columns else {}
+    args_format    = {'template': 'plotly_white', 'height': 750, 'width': 750}
+    args_size      = {'size': 'size', 'size_max': 10} if 'size' in df.columns else {}
+    args_textcolor = ['black' if condition else 'lightgray' for condition in (df['asset']=='SPY')]
     
     fig = (px.scatter(df, 
                       x='dim1', y='dim2', text='asset', color='asset_class', 
-                      **fig_animation,
-                      **fig_format)
+                      **args_size,
+                      **args_animation,
+                      **args_format)
            .update_traces(textposition='middle right', 
-                          textfont_color='lightgray')
+                          textfont_color='lightgray',
+                        #   textfont_color=args_textcolor,
+                          )
            .update_layout(xaxis_title=None,
                           yaxis_title=None,
-                          xaxis_showticklabels=False,
-                          yaxis_showticklabels=False,
+                        #   xaxis_showticklabels=False,
+                        #   yaxis_showticklabels=False,
                         #   xaxis_showgrid=False,
                         #   yaxis_showgrid=False,
                         #   xaxis_showline=True,
                         #   yaxis_showline=True,
+                          xaxis_scaleanchor="y", 
+                          xaxis_scaleratio=1,
+                          yaxis_tickvals = [-0.5, 0, 0.5],
                           legend_title_text=None)
            )
     
+    # fig.update_layout(yaxis_tickvals = fig)
+    
+    # Refactor this into separate function
     if tick_range is not None:
         if tick_range == 'auto':
             tick_range = df[['dim1', 'dim2']].abs().max().max()
-        print(tick_range)
         fig.update_xaxes(range=(-tick_range, tick_range)).update_yaxes(range=(-tick_range, tick_range))
-        
+    
     return fig
 
 
--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Optional, List
 
 import pandas as pd
 import xarray as xr
@@ -23,7 +23,7 @@ def get_yf_data(asset_list: List[str]) -> xr.DataArray:
 
     # return (yf.download(asset_list)
     #         .rename_axis(index='date', columns=['ohlcv_type', 'asset'])
-    #         .rename(index={'Ticker': 'asset'})
+    #         .rename(index={'Ticker': 'asset'}))
     #         .rename(columns=lambda col: col.lower(), level='ohlcv_type')
     #         .stack(['asset', 'ohlcv_type'], future_stack=True)
     #         .to_xarray())
@@ -37,8 +37,19 @@ def get_factor_master(filename: str = 'factor_master.xlsx', sheet_name: str = 'r
     return df
 
 
+def get_portfolios(filename: str = 'portfolios.xlsx', sheet_name: str = 'read', index_col: str = 'portfolio_name') -> pd.DataFrame:
+    df = pd.read_excel(filename, sheet_name=sheet_name, index_col=index_col)
+    df.index = pd.CategoricalIndex(df.index, categories=df.index.unique(), ordered=True)
+    return (df.reset_index()
+            .pivot(index='factor_name', 
+                   columns='portfolio_name', 
+                   values='weight')
+            .fillna(0)
+            )
+
+
 # def get_factor_data(asset_list: List[str], halflifes: List[int]) -> xr.Dataset:
-def get_factor_data(halflifes: List[int] = None) -> xr.Dataset:
+def get_factor_data(halflifes: Optional[List[int]] = None) -> xr.Dataset:
     if halflifes == None:
         halflifes = [21, 63, 126, 252, 512]
     
@@ -54,3 +65,34 @@ def get_factor_data(halflifes: List[int] = None) -> xr.Dataset:
     
     ds['asset'].attrs = factor_master.T
     return ds
+
+
+def align_indices(df1: pd.DataFrame, df2: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Align the indices of two DataFrames to ensure they can be joined without errors.
+    
+    Parameters
+    ----------
+    df1 : pd.DataFrame
+        The first DataFrame.
+    df2 : pd.DataFrame
+        The second DataFrame.
+    
+    Returns
+    -------
+    (pd.DataFrame, pd.DataFrame)
+        The two DataFrames with aligned indices.
+    """
+    common_index = df1.index.union(df2.index)
+    return df1.reindex(common_index), df2.reindex(common_index)
+
+
+# # Example usage:
+# factor_data = get_factor_data()
+# factor_ret = factor_data.ret.to_pandas().fillna(0)
+# portfolios = get_portfolios()
+# portfolio_ret = factor_ret @ portfolios
+
+# # Align indices before joining
+# factor_ret_aligned, portfolio_ret_aligned = align_indices(factor_ret, portfolio_ret)
+# combined_ret = factor_ret_aligned.join(portfolio_ret_aligned, how='outer')
