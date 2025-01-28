7c14d546c85051db5f8d836a3b23494e405c1a09
Ben Kraines <bkraines@gmail.com>
2025-01-28 00:26:09 -0500

    Refactor return functions into py files

--- data.py
+++ data.py
@@ -6,28 +6,37 @@ import xarray as xr
 import yfinance as yf
 
 # from data import get_factor_master, get_yf_data
-from util import xr_pct_change
-from stats import get_volatility_set, get_correlation_set
+from util import xr_pct_change, safe_reindex
+from stats import align_dates, calculate_returns_set, accumulate_returns_set, get_volatility_set, get_correlation_set
 
 
+def get_yahoo_data(ticker, field_name, cache=None):
+    # TODO: Check cache first
+    # cache.columns.get_level_values(1)
+    return yf.download(ticker)[field_name]
+
+
+def get_yahoo_data_set(tickers, field_name, asset_names=None):
+    if asset_names is None:
+        asset_names = tickers
+    return (pd.DataFrame({asset_name: get_yahoo_data(ticker, field_name) 
+                         for asset_name, ticker in zip(asset_names, tickers)})
+            .rename_axis(index='date', columns='factor_name'))
+
+
+# deprecated
 def get_yf_data(asset_list: List[str]) -> xr.DataArray:
+    # TODO: Name the returned datarray 'ohlcv'?
     df = (yf.download(asset_list)
             .rename_axis(index='date', columns=['ohlcv_type', 'asset'])
             .rename(index={'Ticker': 'asset'})
             .rename(columns=lambda col: col.lower(), level='ohlcv_type')
+            # .stack(['asset', 'ohlcv_type'], future_stack=True) # Convert assetst to categorical first
             .stack(['ohlcv_type'], future_stack=True)
             .loc[:, asset_list]
             )
     df.columns = pd.CategoricalIndex(df.columns, categories=asset_list, ordered=True)
     ds = df.stack().to_xarray()
-
-    # return (yf.download(asset_list)
-    #         .rename_axis(index='date', columns=['ohlcv_type', 'asset'])
-    #         .rename(index={'Ticker': 'asset'}))
-    #         .rename(columns=lambda col: col.lower(), level='ohlcv_type')
-    #         .stack(['asset', 'ohlcv_type'], future_stack=True)
-    #         .to_xarray())
-
     return ds
 
 
@@ -48,6 +57,16 @@ def get_portfolios(filename: str = 'factor_master.xlsx', sheet_name: str = 'read
             )
 
 
+def get_yf_returns(asset_list: List[str]) -> xr.Dataset:
+    # TODO: Combine with get_factor_data
+    ds = xr.Dataset()
+    ds['ohlcv'] = get_yf_data(asset_list)
+    ds['cret']  = ds['ohlcv'].sel(ohlcv_type='adj close')
+    ds['ret']   = ds['cret'].ffill(dim='date').pipe(xr_pct_change, 'date')
+    return ds
+
+
+# deprecate
 # def get_factor_data(asset_list: List[str], halflifes: List[int]) -> xr.Dataset:
 def get_factor_data(halflifes: Optional[List[int]] = None) -> xr.Dataset:
     if halflifes == None:
@@ -58,13 +77,77 @@ def get_factor_data(halflifes: Optional[List[int]] = None) -> xr.Dataset:
     
     ds = xr.Dataset()
     ds['ohlcv'] = get_yf_data(asset_list) #.to_dataset(name='ohlcv')
-    ds['cret']  = ds['ohlcv'].sel(ohlcv_type='adj close')
-    ds['ret']   = xr_pct_change(ds['cret'], 'date')
+    
+    # ds['cret']  = ds['ohlcv'].sel(ohlcv_type='adj close')
+    ds['ret']   = (ds['ohlcv']
+                   .sel(ohlcv_type='adj close')
+                   .fill(dim='date')
+                   .pipe(xr_pct_change, 'date'))
     ds['vol']   = get_volatility_set(ds['ret'], halflifes)
     ds['corr']  = get_correlation_set(ds['ret'], halflifes)
     
-    ds['asset'].attrs = factor_master.T
+    ds['asset'].attrs = factor_master.T.to_dict()
     return ds
+
+# deprecate
+def build_dataset_with_composites(halflifes: List[int]) -> xr.Dataset:
+    # Here `assets` refer to factors built from single security (basis vectors)
+    # Here `portfolios` refer to factors built from `assets`
+    # TODO: Rename to get_factor_data_with_composites
+    
+    factor_master = get_factor_master(sheet_name='read')
+    asset_list = factor_master.loc[factor_master['composite'] == 0].index.to_list()
+    asset_data = get_yf_returns(asset_list)
+    asset_ret = asset_data['ret'].fillna(0).to_pandas()
+    
+    portfolios_weights = get_portfolios().pipe(safe_reindex, factor_master).fillna(0).loc[asset_list]  
+    portfolios_ret = asset_ret @ portfolios_weights
+
+    factor_data = xr.Dataset()
+    factor_data['ret']   = pd.concat([asset_ret, portfolios_ret], axis=1).rename_axis(columns='asset')
+    # factor_data['ret'] = (pd.concat([asset_ret['MWTIX'], portfolios_ret, asset_ret.drop(columns=['MWTIX'])], axis=1).rename_axis(columns='asset'))
+    factor_data['vol']   = get_volatility_set(factor_data['ret'], halflifes)
+    factor_data['corr']  = get_correlation_set(factor_data['ret'], halflifes)
+    factor_data['cret']  = factor_data['ret'].cumsum(dim='date') # TODO: Drop zeros from the beginning
+
+    factor_data['asset'].attrs = factor_master.T.to_dict()
+
+    return factor_data
+
+
+def build_factor_data2(halflifes: List[int]) -> xr.Dataset:
+    # TODO: Check vol units
+    factor_master = get_factor_master('factor_master.xlsx', 'read')
+    factor_list = factor_master.index
+
+    factor_list_yf = factor_master.query('source=="yfinance"').index
+    levels_yf = (get_yahoo_data_set(asset_names = factor_list_yf.tolist(), 
+                                     tickers = factor_master.loc[factor_list, 'ticker'],
+                                     field_name = 'Adj Close')
+                 .pipe(align_dates, ['SPY'])
+                 )
+
+    diffusion_map = factor_master['diffusion_type']
+    multiplier_map = factor_master['multiplier']
+    ret_yf = calculate_returns_set(levels_yf, diffusion_map, multiplier_map)
+    
+    portfolios_weights = (get_portfolios()
+                          .pipe(safe_reindex, factor_master)
+                          .fillna(0)
+                          .loc[factor_list_yf]
+                          )
+    portfolios_ret = ret_yf @ portfolios_weights
+    levels_latest = levels_yf.iloc[-1]
+
+    factor_data = xr.Dataset()
+    factor_data['ret']  = pd.concat([ret_yf, portfolios_ret], axis=1).rename_axis(columns='factor_name')
+    factor_data['cret'] = accumulate_returns_set(factor_data['ret'].to_pandas(), diffusion_map, levels_latest, multiplier_map)
+    factor_data['vol']  = get_volatility_set(factor_data['ret'], halflifes)
+    factor_data['corr'] = get_correlation_set(factor_data['ret'], halflifes)
+    factor_data['factor_name'].attrs = factor_master.T.to_dict()
+    
+    return factor_data #, diffusion_map, levels_latest
+
 
 
 def align_indices(df1: pd.DataFrame, df2: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
--- stats.py
+++ stats.py
@@ -1,26 +1,122 @@
 from typing import List
 
-from numpy import sqrt
+from numpy import sqrt, nan
 import pandas as pd
 import xarray as xr
 
 from util import xr_pct_change
 
-def get_ewm_corr(ret: xr.DataArray, halflife: int) -> xr.DataArray:
-    return (ret.to_pandas()
-            .ewm(halflife=halflife)
-            .corr()
-            .rename_axis(columns=lambda name: f"{name}_1")
-            .stack()
-            .rename('corr')
-            .to_xarray())
+
+def fill_returns(df):
+    return df.ffill()
+
+
+def get_business_days(df, factor_names):
+    return df[factor_names].dropna(how='any').index
+
+
+def align_dates(df, business_day_factors):
+    dates_raw = df.index
+    dates_business = get_business_days(df, business_day_factors)
+    dates_union = dates_raw.union(dates_business)
+    return (df
+            .reindex(dates_union)
+            .pipe(fill_returns)
+            .loc[dates_business])
+
+
+def calculate_returns(cret, diffusion_type, multiplier=1e-4):
+    match diffusion_type:
+        case 'lognormal':
+            return cret.pct_change().div(multiplier)
+        case 'normal':
+            return cret.diff().div(multiplier)
+        # case 'normal10':
+        #     return cret.diff().div(10)
+        case _:
+            raise ValueError(f'Unsupported diffusion_type of {diffusion_type} for {cret.name}')
+        # case nan:
+        #     raise ValueError(f'No diffusion_type provided for {cret.name}')
+
+
+def calculate_returns_set(df, diffusion_map, multiplier_map):
+    return (pd.DataFrame({factor: calculate_returns(df[factor], diffusion_map[factor], multiplier_map[factor]) 
+                          for factor in df.columns
+                          })
+            .rename_axis(index='date', columns='factor_name'))
+    
+
+def accumulate_returns_new(ret, diffusion_type, level=None, multiplier=1e-4):
+    # TODO: This drops the first observation
+    if level is None:
+        level = ret.iloc[-1]
+    match diffusion_type:
+        case 'lognormal':
+            cret = ret.mul(multiplier).add(1).cumprod()
+            cret = cret / cret.iloc[-1] * level
+        case 'normal':
+            cret = ret.mul(multiplier).cumsum()
+            cret = cret - cret.iloc[-1] + level
+        case _:
+            raise ValueError(f'Unsupported diffusion_type of {diffusion_type} for {ret.name}')
+    return cret
+
+
+def accumulate_returns_set(ret, diffusion_map, level_map=None, multiplier_map=None):
+    if level_map is None:
+        level_map = {factor: None for factor in ret.columns}  
+    return (pd.DataFrame({factor: accumulate_returns_new(ret = ret[factor], 
+                                                     diffusion_type = diffusion_map[factor], 
+                                                     level = level_map.get(factor, 100), 
+                                                     multiplier = multiplier_map.get(factor, 1e-4)) 
+                          for factor in ret.columns
+                          })
+            .rename_axis(index='date', columns='factor_name'))
+
+
+
+# deprecate
+def accumulate_returns(da_ret: xr.DataArray, dim: str = 'date') -> xr.DataArray:
+    """ 
+    Cumulate returns along a specified dimension, handling NaNs appropriately.
+
+    Parameters
+    ----------
+    da_ret : xarray.DataArray
+        The input data array containing returns to be cumulated.
+    dim : str, optional
+        The dimension along which to cumulate the returns. Default is 'date'.
+
+    Returns
+    -------
+    xarray.DataArray
+        A data array with cumulated returns along the specified dimension.
+        NaNs are preserved at the top of the array but forward-filled after.
+
+    """
+    # TODO: Specify which assets should act as calendar,
+    #       e.g. drop days if SPY or IEF is missing
+    return (da_ret.cumsum(dim=dim)
+            .where(~da_ret.isnull(), nan)
+            .ffill(dim=dim)
+    )
 
 
 def get_volatility_set(ret: xr.DataArray, halflifes: List[int], dim: str='date') -> xr.DataArray:
     # TODO: Use daily 5-day returns
-    return xr.concat([ret.rolling_exp({dim: h}, window_type='halflife').std() * sqrt(252) * 100
+    return xr.concat([(ret/100).rolling_exp({dim: h}, window_type='halflife').std() * sqrt(252)
                       for h in halflifes],
                      dim=pd.Index(halflifes, name='vol_type'))
+
+
+def get_ewm_corr(ret: xr.DataArray, halflife: int) -> xr.DataArray:
+    return (ret.to_pandas()
+            .ewm(halflife=halflife)
+            .corr() # TODO: Try min_periods = halflife * 2
+            .rename_axis(columns=lambda name: f"{name}_1")
+            .stack()
+            .rename('corr')
+            .to_xarray())
 
 
 def get_correlation_set(ret: xr.DataArray, halflifes: List[int]) -> xr.DataArray:
