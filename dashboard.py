91324110c74b2f2b0c16b62f4de5a33ba61c0080
Ben Kraines <bkraines@gmail.com>
2025-02-05 16:04:51 -0500

    Ipdate streamlit test

--- dashboard.py
+++ dashboard.py
@@ -1,56 +1,58 @@
 import streamlit as st
+from util import format_date
+from data import build_factor_data2
+from market_feedback import draw_market_feedback_scatter
 
-import plotly.express as px
-import pandas as pd
-import numpy as np
+halflifes = [21, 63, 126, 252]
 
-# Generate sample data
-np.random.seed(42)
-data = {
-    'x': np.random.randn(50),
-    'y': np.random.randn(50),
-    'category': np.random.choice(['A', 'B', 'C'], size=50)
-}
-df = pd.DataFrame(data)
+@st.cache_data
+def build_factor_data_with_cache(halflifes):
+    return build_factor_data2(halflifes)
 
-# Create scatter plot
-fig = px.scatter(
-    df, 
-    x='x', 
-    y='y', 
-    color='category', 
-    title='Sample Scatter Plot with Plotly',
-    labels={'x': 'X Axis', 'y': 'Y Axis'}
-)
+factor_data = build_factor_data_with_cache(halflifes)
+factor_list = factor_data['factor_name'].values
 
-# Show plot
-# fig.show()
+corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
+return_start = st.date_input('Start', value='2024-12-31') #, on_change)
+return_end   = st.date_input('End', value='today')
+vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
+corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
+return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
+
+fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
+
 st.write(fig)
 
 
+# import streamlit as st
 
-# st.write('hello world ttt')
+# import plotly.express as px
+# import pandas as pd
+# import numpy as np
 
-# from util import format_date
-# from data import build_factor_data2
-# from market_feedback import draw_market_feedback_scatter
+# # Generate sample data
+# np.random.seed(42)
+# data = {
+#     'x': np.random.randn(50),
+#     'y': np.random.randn(50),
+#     'category': np.random.choice(['A', 'B', 'C'], size=50)
+# }
+# df = pd.DataFrame(data)
 
-# halflifes = [21, 63, 126, 252]
+# # Create scatter plot
+# fig = px.scatter(
+#     df, 
+#     x='x', 
+#     y='y', 
+#     color='category', 
+#     title='Sample Scatter Plot with Plotly',
+#     labels={'x': 'X Axis', 'y': 'Y Axis'}
+# )
 
-# @st.cache_data
-# def build_factor_data_with_cache(halflifes):
-#     return build_factor_data2(halflifes)
-
-# factor_data = build_factor_data_with_cache(halflifes)
-# factor_list = factor_data['factor_name'].values
-
-# corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
-# return_start = st.date_input('Start', value='2024-12-31') #, on_change)
-# return_end   = st.date_input('End', value='today')
-# vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
-# corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
-# return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
-
-# fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
-
+# # Show plot
+# # fig.show()
 # st.write(fig)
+
+
+
+# # st.write('hello world ttt')
