fca19dca97ec869a86ec441126a8285aa94da8c0
Ben Kraines <bkraines@gmail.com>
2025-02-11 00:04:08 -0500

    Streamlit test #9

--- dashboard.py
+++ dashboard.py
@@ -1,49 +1,51 @@
-# import streamlit as st
-# from util import format_date
-# from data import build_factor_data2
-# from market_feedback import draw_market_feedback_scatter
-
-# halflifes = [21, 63, 126, 252]
-
-# @st.cache_data
-# def build_factor_data_with_cache(halflifes):
-#     return build_factor_data2(halflifes)
-
-# factor_data = build_factor_data_with_cache(halflifes)
-# factor_list = factor_data['factor_name'].values
-
-# corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
-# return_start = st.date_input('Start', value='2024-12-31') #, on_change)
-# return_end   = st.date_input('End', value='today')
-# vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
-# corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
-# return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
-
-# fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
-
-# st.write(fig)
-
-# TEST 8 - MULTIPLE DROPDOWNS
+# TEST 9 - NO CACHE
 
 import streamlit as st
+from util import format_date
 from data import build_factor_data2
 from market_feedback import draw_market_feedback_scatter
 
-halflifes = [63, 252]
-factor_data = build_factor_data2(halflifes)
+halflifes = [21, 63, 126, 252]
 
+# @st.cache_data
+def build_factor_data_with_cache(halflifes):
+    return build_factor_data2(halflifes)
+
+factor_data = build_factor_data_with_cache(halflifes)
+factor_list = factor_data['factor_name'].values
+
+corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
 return_start = st.date_input('Start', value='2024-12-31') #, on_change)
 return_end   = st.date_input('End', value='today')
 vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
 corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
-# return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
-return_title = f'Returns (std)'
-
-factor_list = factor_data['factor_name'].values
-corr_asset = st.selectbox('Correlation Asset', options=factor_list, index=1)
+return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
 
 fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
+
 st.write(fig)
+
+# TEST 8 - MULTIPLE DROPDOWNS
+
+# import streamlit as st
+# from data import build_factor_data2
+# from market_feedback import draw_market_feedback_scatter
+
+# halflifes = [63, 252]
+# factor_data = build_factor_data2(halflifes)
+
+# return_start = st.date_input('Start', value='2024-12-31') #, on_change)
+# return_end   = st.date_input('End', value='today')
+# vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
+# corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
+# # return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
+# return_title = f'Returns (std)'
+
+# factor_list = factor_data['factor_name'].values
+# corr_asset = st.selectbox('Correlation Asset', options=factor_list, index=1)
+
+# fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
+# st.write(fig)
 
 
 # TEST 7 - SINGLE DROP DOWN
