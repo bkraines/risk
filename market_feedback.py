1ffa5c1e39639a6ed43948a52f31071e0708adec
Ben Kraines <bkraines@gmail.com>
2025-02-10 22:48:21 -0500

    Refactor functions into py file

--- market_feedback.py
+++ market_feedback.py
@@ -1,5 +1,8 @@
+from itertools import product
+
 from numpy import sqrt
 import pandas as pd
+import xarray as xr
 
 import plotly.express as px
 import plotly.io as pio
@@ -115,3 +118,36 @@ def draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type
         line=dict(color='lightgray', width=2)
         )
     return fig
+
+
+def get_intervals(ds: xr.Dataset) -> pd.DataFrame:
+    # TODO: Accept list of intervals, say [5, 21, 63]
+    dates = ds.indexes['date']
+    date_latest = dates.max()
+    year_start = pd.Timestamp(date_latest.year, 1, 1)
+    month_start = pd.Timestamp(date_latest.year, date_latest.month, 1)
+    ytd_start = dates[dates < year_start].max()  # prior_year_end
+    mtd_start = dates[dates < month_start].max() # prior_month_end
+    wow_start = dates[-5-1]                        # week_over_week
+    
+    return (pd.DataFrame(
+                columns = ['name', 'start_date', 'end_date',   'chart_title'],
+                data =   [['ytd',   ytd_start,    date_latest, 'Year-to-Date Return (std)'],
+                          ['mtd',   mtd_start,    date_latest, 'Month-to-Date Return (std)'],
+                          ['5d',    wow_start,    date_latest, '5-Day Return (std)'],
+                          ])
+            .set_index('name'))
+
+
+def draw_market_feedback_scatter_set(factor_data, corr_asset_list, vol_type, corr_type):
+    # TODO: Add output path
+    # TODO: Add dates (and source) to chart
+    intervals = get_intervals(factor_data)
+    interval_list = intervals.index.to_list()
+
+    for corr_asset, interval in product(corr_asset_list, interval_list):
+        i = intervals.loc[interval]
+        fig = draw_market_feedback_scatter(factor_data, i['start_date'], i['end_date'], vol_type, corr_type, corr_asset, i['chart_title'])
+        fig.show(renderer='png')
+        fig.write_image(f'feedback_{corr_asset}_{interval}.png')
+        # fig.write_html(f'feedback_{corr_asset}_{interval}.html')
