d2babfd4a2ac3e341d70a36c9f105a561c5a7ef2
Ben Kraines <bkraines@gmail.com>
2025-02-10 22:53:41 -0500

    Streamlit test #4

--- dashboard.py
+++ dashboard.py
@@ -1,43 +1,43 @@
-import streamlit as st
-from util import format_date
-from data import build_factor_data2
-from market_feedback import draw_market_feedback_scatter
-
-halflifes = [21, 63, 126, 252]
-
-@st.cache_data
-def build_factor_data_with_cache(halflifes):
-    return build_factor_data2(halflifes)
-
-factor_data = build_factor_data_with_cache(halflifes)
-factor_list = factor_data['factor_name'].values
-
-corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
-return_start = st.date_input('Start', value='2024-12-31') #, on_change)
-return_end   = st.date_input('End', value='today')
-vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
-corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
-return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
-
-fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
-
-st.write(fig)
-
-
-### TEST 4 - BIGGER YAHOO CHART
-
 # import streamlit as st
-# import plotly.express as px
-# import pandas as pd
-# import yfinance as yf
-# from data import get_factor_master
+# from util import format_date
+# from data import build_factor_data2
+# from market_feedback import draw_market_feedback_scatter
 
-# factor_master = get_factor_master()
-# factor_list = factor_master.index.values.tolist()
-# data = yf.download(factor_list, start='2020-01-01', end='2023-01-01')['Close']
-# fig = px.line(data)
-# # fig.show()
+# halflifes = [21, 63, 126, 252]
+
+# @st.cache_data
+# def build_factor_data_with_cache(halflifes):
+#     return build_factor_data2(halflifes)
+
+# factor_data = build_factor_data_with_cache(halflifes)
+# factor_list = factor_data['factor_name'].values
+
+# corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
+# return_start = st.date_input('Start', value='2024-12-31') #, on_change)
+# return_end   = st.date_input('End', value='today')
+# vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
+# corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
+# return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
+
+# fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
+
 # st.write(fig)
+
+
+## TEST 4 - BIGGER YAHOO CHART
+
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import yfinance as yf
+from data import get_factor_master
+
+factor_master = get_factor_master()
+factor_list = factor_master.index.values.tolist()
+data = yf.download(factor_list, start='2020-01-01', end='2023-01-01')['Close']
+fig = px.line(data)
+# fig.show()
+st.write(fig)
 
 
 
