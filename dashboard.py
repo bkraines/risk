0fc548a643a16f77970ccb803001019499e5320e
Ben Kraines <bkraines@gmail.com>
2025-02-12 13:09:47 -0500

    Streamlit test #10b

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ from util import format_date
 from data import build_factor_data2
 from market_feedback import draw_market_feedback_scatter
 
-halflifes = [21, 63] #, 126, 252]
+halflifes = [126] # [21, 63, 126, 252]
 
 @st.cache_data
 def build_factor_data_with_cache(halflifes):
@@ -18,8 +18,10 @@ with st.sidebar:
     corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
     return_start = st.date_input('Start', value='2024-12-31') #, on_change)
     return_end   = st.date_input('End', value='today')
-    vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
-    corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
+    # vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
+    # corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
+vol_type  = 126
+corr_type = 126
 
 return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
 fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
