b482a846aafbf25bfc66b3e1a37ad49070c27928
Ben Kraines <bkraines@gmail.com>
2025-03-13 17:11:00 -0400

    Debug off-by-one error to fix streamlit dahsboard

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 IMAGE_DIR = 'images'
 CACHE_DIR = 'cache'
+ARRAYLAKE_REPO = 'finance-demos/demo-icechunk'
 
 STREAMLIT_CACHE = True
+HALFLIFES = [21, 63, 126, 252, 512]
+
--- feedback.py
+++ feedback.py
@@ -2,12 +2,12 @@ import streamlit as st
 from util import format_date, check_memory_usage, summarize_memory_usage
 from data import get_factor_data
 from market_feedback import draw_market_feedback_scatter
-from config import STREAMLIT_CACHE
+from config import STREAMLIT_CACHE, HALFLIFES
 
 if STREAMLIT_CACHE:
     halflifes = [126] # [21, 63, 126, 252]
 else:
-    halflifes = [21, 63, 126, 252]
+    halflifes = HALFLIFES
 
 # @st.cache_data
 # def get_factor_data_streamlit(halflifes):
@@ -19,13 +19,11 @@ def build_streamlit_dashboard(factor_data):
     factor_list = factor_data['factor_name'].values
 
     with st.sidebar:
-        corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=1)
+        corr_asset   = st.selectbox('Correlation Asset', options=factor_list, index=0)
         return_start = st.date_input('Start', value='2024-12-31') #, on_change)
         return_end   = st.date_input('End', value='today')
-        vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=1)
-        corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=1)
-    # vol_type  = 126
-    # corr_type = 126
+        vol_type     = st.selectbox('Volatility Halflife', options=halflifes, index=0)
+        corr_type    = st.selectbox('Correlation Halflife', options=halflifes, index=0)
 
     return_title = f'Returns from {format_date(return_start)} to {format_date(return_end)} (std)'
     fig = draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title)
