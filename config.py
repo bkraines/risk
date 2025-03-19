aec6873f303ed7f9c90c0981b183e59e1b803f92
Ben Kraines <bkraines@gmail.com>
2025-03-19 16:25:56 -0400

    Prune old code

--- config.py
+++ config.py
@@ -2,12 +2,14 @@ from typing import Literal
 
 IMAGE_DIR = 'images'
 CACHE_DIR = 'cache'
+CACHE_FILENAME = 'factor_data.zarr'
 ARRAYLAKE_REPO = 'finance-demos/demo-icechunk'
 
 CACHE_TARGET: Literal['disk', 'arraylake', 'streamlit'] = 'streamlit'
 STREAMLIT_CACHE = False
 
-# Limit RAM usage until streamlit is hooked up to arraylake:
+# In case of `streamlit` caching, limit RAM usage by restricting `HALFLIFES`
+# Unnecessary after `arraylake` caching is fully implemented
 if STREAMLIT_CACHE or (CACHE_TARGET == 'streamlit'):
     HALFLIFES = [126]
 else:
--- dashboard_pages.py
+++ dashboard_pages.py
@@ -2,20 +2,22 @@ import streamlit as st
 
 from config import HALFLIFES
 
-from data import build_factor_data
+from data import get_factor_data
 from feedback import build_streamlit_dashboard as market_feedback
 from timeseries import build_dashboard_vol as time_series
 from corr_mds_dashboard import build_streamlit_dashboard as corr_mds
 
 
-factor_data = build_factor_data(HALFLIFES) #, streamlit=STREAMLIT_CACHE)
+factor_data = get_factor_data()
 
 pg = st.navigation([st.Page(lambda: market_feedback(factor_data), 
                             title='Market Feedback',
                             url_path='feedback'), 
+                    
                     st.Page(lambda: time_series(factor_data), 
                             title='Time Series',
                             url_path='timeseries'),
+                    
                     st.Page(lambda: corr_mds(factor_data),
                             title='Correlation Projection',
                             url_path='correlation'),
