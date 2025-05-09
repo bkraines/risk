e020363c8c07f643c878f9ac503acd87dd579f09
Ben Kraines <bkraines@gmail.com>
2025-05-09 01:02:42 -0400

    test: Display full factor returns for Heroku

--- dashboard_heroku.py
+++ dashboard_heroku.py
@@ -6,4 +6,4 @@ from risk_data import get_factor_master, get_factor_data
 # st.dataframe(factor_master)
 
 factor_data = get_factor_data(read_cache=False)
-st.dataframe(factor_data.ret.to_pandas())
+st.dataframe(factor_data.ret.to_pandas().tail(60))
