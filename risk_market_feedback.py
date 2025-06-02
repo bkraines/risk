a56ed800f6ac460dbdb506bf0016cedb9400263c
Ben Kraines <bkraines@gmail.com>
2025-06-02 11:06:54 -0400

    feature: Include factor descriptions in market feedback hovertext

--- risk_market_feedback.py
+++ risk_market_feedback.py
@@ -10,8 +10,7 @@ from risk_chart import px_scatter, px_write
 # TODO: Add chart of showing correlation at t and t-n
 
 def draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title):
-    # TODO: Accomodate MTD, YTD, date interval, date range, and single day
-    # TODO: Move MWTIX renaming to factor_master
+    # TODO: Rename 'asset' to 'factor' in dataframe
     
     ndays = factor_data['cret'].sel(date=slice(return_start, return_end)).date.size - 1  # - 1
     corr  = (factor_data['corr']
@@ -32,10 +31,10 @@ def draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type
 
     factor_master = pd.DataFrame(factor_data['factor_name'].attrs).T
 
-    df = (pd.concat([corr, zscore, factor_master['asset_class'], factor_master['hyper_factor']], axis=1)
-          .replace('MWTIX', 'TCW')
+    df = (pd.concat([corr, zscore, factor_master[['asset_class', 'hyper_factor', 'description']]], axis=1)
+          # .replace('MWTIX', 'TCW')
           .rename_axis('asset').reset_index()
-          .replace('MWTIX', 'TCW')
+          # .replace('MWTIX', 'TCW')
         #   .assign(size = lambda df: df['hyper_factor'].apply(lambda x: 10 if x == 1 else 1).astype('float'))
           )
     
@@ -44,7 +43,8 @@ def draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type
     color_map_override = {'Portfolio': 'black', 
                           'Theme':     'red'}
     fig = (px_scatter(df, x='corr', y='zscore', text='asset', color='asset_class', #size='size',
-                      color_map_override = color_map_override)
+                      color_map_override = color_map_override,
+                      hover_data={'description': True})
            .update_layout(yaxis_title=return_title,
                           xaxis_title=f'Correlation with {corr_asset}')
            .update_xaxes(showgrid=True, 
@@ -64,82 +64,6 @@ def draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type
                         #   legend_xanchor="right", ) )
                         #   legend_x=1)
                     
-    # from risk_chart import px_format
-    # fig = px_format(fig, 
-    #                 x_title='Correlation with 10-year bond',
-    #                 y_title='Return on Fed Announcement (std)')
-
-    corr_min = df['corr'].min()
-    fig.add_shape(
-        type="line",
-        x0=df[df['asset'] == corr_asset]['corr'].values[0] * corr_min,
-        y0=df[df['asset'] == corr_asset]['zscore'].values[0] * corr_min,
-        x1=df[df['asset'] == corr_asset]['corr'].values[0],
-        y1=df[df['asset'] == corr_asset]['zscore'].values[0],
-        line=dict(color='lightgray', width=2)
-        )
-    return fig
-
-
-def draw_market_feedback_scatter(factor_data, return_start, return_end, vol_type, corr_type, corr_asset, return_title):
-    # TODO: Accomodate MTD, YTD, date interval, date range, and single day
-    # TODO: Move MWTIX renaming to factor_master
-    
-    ndays = factor_data['cret'].sel(date=slice(return_start, return_end)).date.size - 1  # - 1
-    corr  = (factor_data['corr']
-             .shift(date=1)
-             .sel(corr_type=corr_type, factor_name_1=corr_asset)
-             .sel(date=return_start, method='ffill')
-             .to_series()
-             )
-    vol   = (factor_data['vol']
-             .shift(date=1)
-             .sel(vol_type=vol_type)
-             .sel(date=return_start, method='ffill')
-             .to_series()
-             .mul(sqrt(ndays / 252) * 100)
-             )
-    ret = total_return(factor_data['cret'], return_start, return_end)
-    zscore = ret.div(vol).rename('zscore')
-
-    factor_master = pd.DataFrame(factor_data['factor_name'].attrs).T
-
-    df = (pd.concat([corr, zscore, factor_master['asset_class'], factor_master['hyper_factor']], axis=1)
-          .replace('MWTIX', 'TCW')
-          .rename_axis('asset').reset_index()
-          .replace('MWTIX', 'TCW')
-        #   .assign(size = lambda df: df['hyper_factor'].apply(lambda x: 10 if x == 1 else 1).astype('float'))
-          )
-    
-    # n = len(df.asset_class.unique())
-    
-    color_map_override = {'Portfolio': 'black', 
-                          'Theme':     'red'}
-    fig = (px_scatter(df, x='corr', y='zscore', text='asset', color='asset_class', #size='size',
-                      color_map_override = color_map_override)
-           .update_layout(yaxis_title=return_title,
-                          xaxis_title=f'Correlation with {corr_asset}')
-           .update_xaxes(showgrid=True, 
-                         tick0=0,
-                         dtick=0.25, 
-                         zeroline=True,
-                         zerolinecolor='lightgray')
-           .update_yaxes(zeroline=True, 
-                         zerolinecolor='lightgray')
-           .update_layout(legend_orientation="h", 
-                          legend_yanchor="top", 
-                          legend_y=1.06, 
-                        #   legend_entrywidthmode='fraction',
-                        #   legend_entrywidth=1/(n+1),
-                        #   legend_entrywidth=40,
-                          ))
-                        #   legend_xanchor="right", ) )
-                        #   legend_x=1)
-                    
-    # from risk_chart import px_format
-    # fig = px_format(fig, 
-    #                 x_title='Correlation with 10-year bond',
-    #                 y_title='Return on Fed Announcement (std)')
 
     corr_min = df['corr'].min()
     fig.add_shape(
